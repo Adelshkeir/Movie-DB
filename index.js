50d01f3d3cf194077c501dbfd10722fef632c3e9
Adelshkeir <adelshkeir12@gmail.com>
2023-09-23 14:24:12 +0300

    step4

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ let hours = currentTime.getHours();
 let minutes = currentTime.getMinutes();
 let time = hours + ":" + minutes;
 
+
 app.get('/test', (req, res) => {
     res.send({status:200, message:"ok"});
   });
   app.get('/time', (req, res) => {
     res.send({status:200, message:time});
+  });
+  app.get('/hello/:ID', (req, res) => {
+    const ID  = req.params.ID
+    res.send({status:200, message:ID});
+  });
+  app.get('/search', (req, res) => {
+    const SEARCH = req.query.s;
+    if (SEARCH !== "") {
+      res.send({ status: 200, message: "ok", data: SEARCH });
+    } else {
+      res.status(500)
+      res.send({ status: 500, error: true, message: "you have to provide a search" });
+    }
   });
 
 app.listen(3000);
